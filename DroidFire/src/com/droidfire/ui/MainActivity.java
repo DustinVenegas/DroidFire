bdd240ca9f719700df34d5ffb628c45576f4c200
Corey Sunwold <csunwold@gmail.com>
2011-11-16 20:00:16 -0800

    Checking for existing credentials

--- DroidFire/src/com/droidfire/ui/MainActivity.java
+++ DroidFire/src/com/droidfire/ui/MainActivity.java
@@ -1,9 +1,9 @@
 package com.droidfire.ui;
 
 import com.droidfire.R;
-import com.droidfire.R.layout;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
@@ -11,6 +11,18 @@ public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        
+        //Check to see if user token and url are stored
+        
+        SharedPreferences credentials = this.getSharedPreferences("credentials", MODE_PRIVATE);
+        String token = credentials.getString("token", "");
+        if (token != "") {  //If we have the information, send on to the room list
+        	
+        } else { 			//If we dont, show the login information
+        	
+        }
+        
         setContentView(R.layout.main);
     }
 }
