dee9270fad59e4a8a80ae41f25ccb62ca41c3137
Corey Sunwold <csunwold@gmail.com>
2011-12-14 22:02:50 -0800

    Attempting login, if successful or we have credentials show the rooms

--- DroidFire/src/com/droidfire/ui/Main.java
+++ DroidFire/src/com/droidfire/ui/Main.java
@@ -1,10 +1,12 @@
 package com.droidfire.ui;
 
 import com.droidfire.R;
+import com.droidfire.campfire.Site;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -25,15 +27,12 @@ public class Main extends Activity implements OnClickListener {
         //Check to see if user token and url are stored        
         SharedPreferences credentials = this.getSharedPreferences("credentials", MODE_PRIVATE);
         String token = credentials.getString("token", "");
-        if (token == "") {  //If we dont, show the login information
+        if (token.equals("")) {  //If we dont, show the login information
         	setContentView(R.layout.main);
         	Button loginButton = (Button)findViewById(R.id.loginButton);
         	loginButton.setOnClickListener(this);
         } else { 			//If we have the information, send on to the room list
-        	Intent loadRooms = new Intent(this, Rooms.class);
-        	loadRooms.putExtra("token", token);
-        	startActivity(loadRooms);
-        	this.finish();
+        	showRoomsList(token);
         }        
     }
 
@@ -41,10 +40,23 @@ public class Main extends Activity implements OnClickListener {
 	public void onClick(View view) {
 		switch(view.getId()) {
 		case R.id.loginButton:
-			
+			Site site = new Site(Uri.parse("http://" + mSiteName.getText() + ".campfirenow.com"));
+			String token = site.login(mUsername.getText().toString(), mPassword.getText().toString());
+			if (!token.equals("")) { 	//Successful login
+				showRoomsList(token);
+			} else {					//Unsuccessful login
+				
+			}
 			break;
 		default:
 			break;
 		}		
+	}
+	
+	private void showRoomsList(String token) {
+		Intent loadRooms = new Intent(this, Rooms.class);
+    	loadRooms.putExtra("token", token);
+    	startActivity(loadRooms);
+    	this.finish();
 	}
 }
