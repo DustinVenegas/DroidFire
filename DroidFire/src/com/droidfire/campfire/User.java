e60d66ca6894c9d96af10be40f2448a86e9d2e75
Corey Sunwold <csunwold@gmail.com>
2011-12-14 22:21:46 -0800

    Adding user with bare properties

---
+++ DroidFire/src/com/droidfire/campfire/User.java
@@ -0,0 +1,24 @@
+package com.droidfire.campfire;
+
+public class User {
+	private boolean mIsAdmin;
+	private String mEmail;
+	private int mId;
+	private String mName;
+	
+	public boolean isAdmin() {
+		return mIsAdmin;
+	}
+	
+	public String getEmail() {
+		return mEmail;
+	}
+	
+	public int getId() {
+		return mId;
+	}
+	
+	public String getName() {
+		return mName;
+	}
+}
