931e1289765036285d7ba4eb3abc9c51b8919e04
Corey Sunwold <csunwold@gmail.com>
2011-12-14 21:25:53 -0800

    changed MainActivity to Main

--- DroidFire/src/com/droidfire/ui/Main.java
+++ DroidFire/src/com/droidfire/ui/Main.java
@@ -9,7 +9,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class MainActivity extends Activity implements OnClickListener {
+public class Main extends Activity implements OnClickListener {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
