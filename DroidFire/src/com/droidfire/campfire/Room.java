c539a8f84c9cd0a0d48ea4963539d8d18215f5e7
Corey Sunwold <csunwold@gmail.com>
2011-12-14 22:19:39 -0800

    Adding limited properties to Room

--- DroidFire/src/com/droidfire/campfire/Room.java
+++ DroidFire/src/com/droidfire/campfire/Room.java
@@ -2,7 +2,23 @@ package com.droidfire.campfire;
 
 public class Room {
 	
+	private int mId;
+	private String mName;
+	private String mTopic;
+		
 	Room(String token) {
 		
+	}
+	
+	public int getId() {
+		return mId;
+	}
+	
+	public String getName() {
+		return mName;
+	}
+	
+	public String getTopic() {
+		return mTopic;
 	}
 }
