f378d66828f0e324562d9b008516ce64749292fe
Corey Sunwold <csunwold@gmail.com>
2011-12-14 21:33:59 -0800

    Adding constructors and private token property

--- DroidFire/src/com/droidfire/campfire/Site.java
+++ DroidFire/src/com/droidfire/campfire/Site.java
@@ -2,6 +2,16 @@ package com.droidfire.campfire;
 
 public class Site {
 	
+	private String mToken;
+	
+	public Site() {
+		
+	}
+	
+	public Site(String token) {
+		mToken = token;
+	}
+	
 	public String login(String userName, String password) {
 		throw new UnsupportedOperationException();
 	}
