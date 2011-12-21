b6bfe5cbb6e0432be5e7c81259369dc3701ebd65
Corey Sunwold <csunwold@gmail.com>
2011-12-21 15:30:35 -0800

    Added test for getRooms method

--- DroidFireTest/src/com/droidfire/test/SiteTest.java
+++ DroidFireTest/src/com/droidfire/test/SiteTest.java
@@ -1,7 +1,11 @@
 package com.droidfire.test;
 
 import java.net.URI;
+import java.util.List;
 
+import android.util.Log;
+
+import com.droidfire.campfire.Room;
 import com.droidfire.campfire.Site;
 
 import junit.framework.TestCase;
@@ -24,6 +28,21 @@ public class SiteTest extends TestCase {
 		Site site = new Site(URI.create("https://sample.campfirenow.com"));
 		String response = site.login("", "");
 		assertTrue(response != null);
+		
+		Log.i("response token", response);
+	}
+	
+	public final void testGetRooms() {
+		Site site = new Site(URI.create("https://sample.campfirenow.com"));
+		String token = site.login("", "");
+		assertTrue(token != null);
+		
+		List<Room> rooms = site.getRooms();
+		assertTrue(rooms != null);
+		assertTrue(rooms.size() > 0);
+		
+		Log.i("first room id", rooms.get(0).getId() + "");
+		
 	}
 
 }
