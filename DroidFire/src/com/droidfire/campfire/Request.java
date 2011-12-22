46a9cf1052bc7364a2b3f609f9ba86f512523dea
Corey Sunwold <csunwold@gmail.com>
2011-12-21 16:55:10 -0800

    Adding post method

--- DroidFire/src/com/droidfire/campfire/Request.java
+++ DroidFire/src/com/droidfire/campfire/Request.java
@@ -12,6 +12,8 @@ import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -48,6 +50,42 @@ class Request {
 		
 		Response result = null;
 		try {
+			HttpResponse httpResponse = client.execute(targetHost, request);
+			JSONObject data = serializeToJson(httpResponse.getEntity().getContent());
+			result = new Response(data, httpResponse.getStatusLine().getStatusCode());
+			
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();			
+		}
+
+		return result;
+	}
+
+	public Response post(URI target, JSONObject json) {
+		HttpHost targetHost = new HttpHost(target.getHost(), target.getPort(), target.getScheme());
+		DefaultHttpClient client = new DefaultHttpClient();
+		if (token == null || token.equals("")) {
+			client.getCredentialsProvider().setCredentials(
+					new AuthScope(targetHost.getHostName(), targetHost.getPort()), 
+					new UsernamePasswordCredentials(mUsername, mPassword));
+		} else {
+			client.getCredentialsProvider().setCredentials(
+					new AuthScope(targetHost.getHostName(), targetHost.getPort()), 
+					new UsernamePasswordCredentials(token, "X"));
+		}
+		
+		Response result = null;
+		try {
+			HttpPost request = new HttpPost(target);
+			request.setHeader("Content-Type", "application/json");
+			StringEntity entity = new StringEntity(json.toString());
+			entity.setContentType("text/json");
+			request.setEntity(entity);
+			
 			HttpResponse httpResponse = client.execute(targetHost, request);
 			JSONObject data = serializeToJson(httpResponse.getEntity().getContent());
 			result = new Response(data, httpResponse.getStatusLine().getStatusCode());
